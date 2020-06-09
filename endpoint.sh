83dbb64e4db88527996591970d4f5f30dc4face6
Sasha Rush <sasha.rush@gmail.com>
2020-06-09 18:43:00 -0400

    Update endpoint.sh

--- endpoint.sh
+++ endpoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Make sure that database is owned by user mongodb
+chown -R mongodb /data/db
+
 # Initialize replicaSet
 /init_repl.sh || exit $?
 
-# Make sure that database is owned by user mongodb
-chown -R mongodb /data/db
+
 
 exec su -s /bin/sh -c "exec mongod -smallfiles --oplogSize 128 --storageEngine=mmapv1 --bind_ip_all --replSet rs0" mongodb 
