cadc0aba0f40b6d72771b35d773f6402c7914bfc
Sasha Rush <sasha.rush@gmail.com>
2020-06-09 14:56:34 -0400

    Create endpoint.sh

---
+++ endpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Initialize replicaSet
+/init_repl.sh || exit $?
+
+# Make sure that database is owned by user mongodb
+[ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
+
+exec su -s /bin/sh -c "exec mongod -smallfiles --oplogSize 128 --storageEngine=mmapv1 --bind_ip_all --replSet rs0" mongodb 
